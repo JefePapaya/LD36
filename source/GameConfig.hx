31c95fca6543b6c3df8216c6aa693e3e1cf6b3a3
publicidadeba <publicidadeba@gmail.com>
2016-08-27 13:22:25 -0300

    Velocidade para 50
    Agora sim!!

--- source/GameConfig.hx
+++ source/GameConfig.hx
@@ -2,7 +2,7 @@ package;
 
 class GameConfig {
 	//Player
-	public static var playerSpeed:Float = 200;
+	public static var playerSpeed:Float = 50;
 	public static var playerDrag:Float = 1600;
 	//Map generation
 
